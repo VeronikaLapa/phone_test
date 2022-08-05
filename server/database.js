fc135ff61782a8ad6213360117ca4f7171d3c4e1
VeronikaLapa <Lapshina.nika@yandex.ru>
2022-08-06 01:34:45 +0300

    Move environment variables to .env config. Example of .env file

--- server/database.js
+++ server/database.js
@@ -1,25 +1,25 @@
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(
-    "postgres://postgres:postgres@localhost:5432/postgres"
+  `postgres://${process.env.DB_USER}:${process.env.DB_PSW}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_SCHEME}`
 );
 
 const Phone = sequelize.define("Phone", {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-    },
-    code: {
-        type: Sequelize.STRING(4),
-        allowNull: false,
-    },
-    number: {
-        type: Sequelize.STRING(10),
-        allowNull: false,
-    },
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  code: {
+    type: Sequelize.STRING(4),
+    allowNull: false,
+  },
+  number: {
+    type: Sequelize.STRING(10),
+    allowNull: false,
+  },
 });
 
 module.exports = {
-    sequelize: sequelize,
-    Phone: Phone,
+  sequelize: sequelize,
+  Phone: Phone,
 };
--- server/index.js
+++ server/index.js
@@ -1,55 +1,59 @@
-var db = require('./database.js');
-db.sequelize.sync({force: true})
+require('dotenv').config();
 
-ws = require('./webSocket');
-const port = 7000;
-const host = "localhost";
-const express = require('express'),
-    app = express();
-const cors = require('cors')
-const bodyParser = require('body-parser');
-app.use(cors())
-app.use(bodyParser.json());
+var db = require("./database.js");
+db.sequelize.sync({ force: true });
+
+ws = require("./webSocket");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express().use(cors()).use(bodyParser.json());
+
+const port = parseInt(process.env.WS_PORT);
+const host = process.env.WS_HOST;
 app.listen(port, host, () =>
-    console.log(`Server listens http://${host}:${port}`)
-)
-ws.wsServer.on('request', function(request) {
-    const connectionInfo = ws.connectUser(request);
-    connectionInfo.connection.on("message", function (message) {
-      if (message.type === "utf8") {
-        const dataFromClient = JSON.parse(message.utf8Data);
-        console.log(dataFromClient);
-      }
-    });
-    connectionInfo.connection.on('close', function() {
-        ws.disconnectUser(connectionInfo.userID);
+  console.log(`Server listens http://${host}:${port}`)
+);
+
+ws.wsServer.on("request", function (request) {
+  const connectionInfo = ws.connectUser(request);
+  connectionInfo.connection.on("message", function (message) {
+    if (message.type === "utf8") {
+      const dataFromClient = JSON.parse(message.utf8Data);
+      console.log(dataFromClient);
+    }
+  });
+  connectionInfo.connection.on("close", function () {
+    ws.disconnectUser(connectionInfo.userID);
+  });
+  connectionInfo.connection.on("error", function (err) {
+    console.log("Error for user" + connectionInfo.userID);
+  });
+});
+app.get("/phones", (req, res) => {
+  db.Phone.findAll({
+    order: [["id", "DESC"]],
+    attributes: ["id", "code", "number"],
+  })
+    .then((allPhones) => {
+      res.send(allPhones);
+    })
+    .catch((err) => {
+      res.error();
     });
 });
-app.get('/phones', (req, res) => {
-    db.Phone.findAll({
-        order: [["id", "DESC"]],
-        attributes: ["id", "code", "number"]
-    }).then((allPhones) => {
-        res.send(allPhones);
+app.post("/phones", (req, res) => {
+  const code = req.body.code;
+  const number = req.body.number;
+  db.Phone.create({
+    code: code,
+    number: number,
+  }).then((user) => {
+      ws.sendMessage(JSON.stringify({code: user.code, number: user.number, id: user.id}));
+      res.send(user);
     })
-})
-app.post('/phones', (req, res) => {
-    console.log(req);
-    const code = req.body.code;
-    const number = req.body.number;
-    db.Phone.create({
-        code: code,
-        number: number,
-    }).then((user) => {
-        ws.sendMessage(JSON.stringify({code, number}));
-        res.send(user);
-    }).catch((err) => {
-        res.error();
+    .catch((err) => {
+      res.error();
     });
-
-})
-
-
-
-
-
+});
