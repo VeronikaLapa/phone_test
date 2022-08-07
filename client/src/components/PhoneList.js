fd9921692fc4c20946a97e33d7bc01c3ba621252
VeronikaLapa <Lapshina.nika@yandex.ru>
2022-08-07 17:12:35 +0300

    PhoneList style

--- client/src/components/PhoneList.js
+++ client/src/components/PhoneList.js
@@ -1,17 +1,21 @@
 import {useSelector} from "react-redux";
 import {selectList} from "../store";
+import styles from "./PhoneList.module.css";
 
 export default function PhoneList() {
     const phoneList = useSelector(selectList);
     return (
-        <ol>
-            {phoneList.map((phone) => {
-                return (
-                    <li key={phone.id}>
-                        {phone.code} {phone.number}
-                    </li>
-                );
-            })}
-        </ol>
+        <div className={styles.container}>
+            <span className={styles.list_header}>Entered phone numbers:</span>
+            <ul className={styles.list}>
+                {phoneList.map((phone) => {
+                    return (
+                        <li key={phone.id} className={styles.list_item}>
+                            {phone.code} {phone.number}
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
     );
 }
